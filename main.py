426da45c946a3b0fe3b10e9e6573f62d375a3b35
hyoeun05 <khe4757628@gmail.com>
2025-05-19 07:35:18 +0000

    응용 예제

---
+++ main.py
@@ -0,0 +1,12 @@
+# blog.py 에서 함수 만들어서 모듈 포함시켜서 활용
+import blog
+
+# 여기에서 메인 프로그램 작성
+# 맛집 검색
+# 맛집을 입력하면
+# 결과가 나오는 프로그램 만들기
+
+# n = input("1. 블로그 검색")
+search = input("맛집 검색: ")
+blog.nblog(search)
+
